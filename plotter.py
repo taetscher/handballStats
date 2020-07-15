78e758d1904c83c95efffc740defa81ffa9af426
taetscher <auteblauwau@hotmail.com>
2020-07-15 15:56:53 +0200

    dealt with pandas DataError, added TVS3

--- plotter.py
+++ plotter.py
@@ -1,49 +1,64 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+from pandas.core.groupby.groupby import DataError
+import os
 
-############## SET INFILE VARIABLE TO GROUP ###############
+def plot():
+    indir = os.listdir('scraped_data')
 
-infile = 'Junioren_U19_Elite_201920'
-group = infile.split('_')[1:]
-s = '_'
-group = s.join(group)
+    for file in indir:
+        infile = file[:-4]
+        group = infile.split('_')[1:]
+        s = '_'
+        group = s.join(group)
+        print('\n\n\n')
+        print('reading in... ' + infile
+              )
 
-############## SET INFILE VARIABLE TO GROUP ###############
+        df = pd.read_csv('scraped_data/' + infile + '.csv')
+        # df = df.drop(['yellowCards','2mins','redCards'], axis=1 )
 
-df = pd.read_csv('scraped_data/' + infile + '.csv')
-#df = df.drop(['yellowCards','2mins','redCards'], axis=1 )
+        try:
+            goalsPerGame = df.groupby(['team'])['goalsPerGame'].mean().sort_values(ascending=False)
+            suspensions = df.groupby(['team'])['2mins'].mean().sort_values(ascending=False)
+            yellows = df.groupby(['team'])['yellowCards'].mean().sort_values(ascending=False)
+            reds = df.groupby(['team'])['redCards'].sum().sort_values(ascending=False)
 
-goalsPerGame = df.groupby(['team'])['goalsPerGame'].aggregate(np.mean).sort_values(ascending=False)
-suspensions = df.groupby(['team'])['2mins'].mean().sort_values(ascending=False)
-yellows = df.groupby(['team'])['yellowCards'].mean().sort_values(ascending=False)
-reds = df.groupby(['team'])['redCards'].sum().sort_values(ascending=False)
+            # print(goalsPerGame.head(10))
+            print(suspensions.head(20))
+            print(yellows.head(20))
+            print(reds.head(20))
 
-#print(goalsPerGame.head(10))
-print(suspensions.head(20))
-print(yellows.head(20))
-print(reds.head(20))
+            goalsPerGame.plot(x='teams', y='goalsPerGame', kind='bar')
+            plt.title('Mean amount of Goals per Player per Team per Game\n{}'.format(group))
+            plt.tight_layout()
+            plt.savefig('output_png/{}_meanGoalsPerPlayerPerTeamPerGame.png'.format(group))
+            plt.close('all')
 
-goalsPerGame.plot(x='teams',y='goalsPerGame',kind='bar')
-plt.title('Mean amount of Goals per Player per Team per Game\n{}'.format(group))
-plt.tight_layout()
-plt.savefig('output_png/{}_meanGoalsPerPlayerPerTeamPerGame.png'.format(group))
-plt.close('all')
+            suspensions.plot(x='teams', y='2mins', kind='bar')
+            plt.title('Mean amount of Suspensions per Player per Team\n{}'.format(group))
+            plt.tight_layout()
+            plt.savefig('output_png/{}_meanSuspensionsPerPlayerPerTeam.png'.format(group))
+            plt.close('all')
 
-suspensions.plot(x='teams',y='2mins',kind='bar')
-plt.title('Mean amount of Suspensions per Player per Team\n{}'.format(group))
-plt.tight_layout()
-plt.savefig('output_png/{}_meanSuspensionsPerPlayerPerTeam.png'.format(group))
-plt.close('all')
+            yellows.plot(x='teams', y='yellowCards', kind='bar')
+            plt.title('Mean amount of Yellow Cards per Player per Team\n{}'.format(group))
+            plt.tight_layout()
+            plt.savefig('output_png/{}_meanYellowCardsPerPlayerPerTeam.png'.format(group))
+            plt.close('all')
 
-yellows.plot(x='teams',y='yellowCards',kind='bar')
-plt.title('Mean amount of Yellow Cards per Player per Team\n{}'.format(group))
-plt.tight_layout()
-plt.savefig('output_png/{}_meanYellowCardsPerPlayerPerTeam.png'.format(group))
-plt.close('all')
+            reds.plot(x='teams', y='redCards', kind='bar')
+            plt.title('Sum of Red Cards per Team\n{}'.format(group))
+            plt.tight_layout()
+            plt.savefig('output_png/{}_redCardsPerTeam.png'.format(group))
+            plt.close('all')
 
-reds.plot(x='teams',y='redCards',kind='bar')
-plt.title('Sum of Red Cards per Team\n{}'.format(group))
-plt.tight_layout()
-plt.savefig('output_png/{}_redCardsPerTeam.png'.format(group))
-plt.close('all')
+        except DataError as e:
+            print('ERROR: {}'.format(e))
+            print('The season you are looking at probably does not have player stats (yet/anymore)')
+            print('Looked specifically at group {}'.format(group))
+            print('\n\n\n')
+            continue
+
+if __name__ == '__main__':
+    plot()
--- scraper.py
+++ scraper.py
@@ -1,45 +1,60 @@
 from selenium import webdriver
 import time
 
+#set groups (2019/2020, groups were 12135 (u19), 12136 (u17), 12137 (u15)
+seasons = {'LakeSide_Elite_1920':[12135,12136,12137],'LakeSide_Elite_2021':[12954,12955,12956],'TVS_3_1920':[12147]}
+groups = []
+
 def scrape():
-    # specify url
-    urlpage = 'https://www.handball.ch/de/matchcenter/gruppen/12135#/stats'
-    print('scraping... ', urlpage)
+    for value in seasons.values():
+        groups.extend(value)
 
-    # run firefox webdriver from executable path of your choice
-    driver = webdriver.Firefox(executable_path='/Users/benischuepbach/Desktop/Coding/sources/geckodriver')
+    print('scraping from groups...', groups)
 
-    driver.get(urlpage)
-    driver.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    time.sleep(5)
+    for group in groups:
+        # specify url
+        urlpage = 'https://www.handball.ch/de/matchcenter/gruppen/{}#/stats'.format(group)
+        print('scraping... ', urlpage)
 
-    results = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table')
-    group =  driver.find_element_by_xpath('/html/body/div[3]/div[3]/div/div/div[2]/p').text.replace(' ','_')
-    season = driver.find_element_by_xpath('//*[@id="menu-breadcrumb-season-trigger"]/span').text.replace('/','')
+        # run firefox webdriver from executable path of your choice
+        driver = webdriver.Firefox(executable_path='/Users/benischuepbach/Desktop/Coding/sources/geckodriver')
 
+        driver.get(urlpage)
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        time.sleep(5)
 
-    teams = []
+        results = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table')
+        group = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div/div/div[2]/p').text.replace(' ', '_')
+        season = driver.find_element_by_xpath('//*[@id="menu-breadcrumb-season-trigger"]/span').text.replace('/', '')
 
-    data = results.text.replace('\n', '\n').replace(' ',',')
+        teams = []
 
-    with open('scraped_data/{}_{}.csv'.format(group,season), 'w') as outfile:
-        outfile.write('playerName,team,games,goalsPerGame,yellowCards,2mins,redCards\n')
-        outfile.close()
+        data = results.text.replace('\n', '\n').replace(' ', ',')
 
-    for n in range(1,len(data.split('\n'))):
-            player_name = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[2]'.format(n))
-            player_team = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[3]'.format(n))
-            player_games = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[6]'.format(n))
-            player_gpg = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[7]'.format(n))
-            player_yellow = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[8]'.format(n))
-            player_2min = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[9]'.format(n))
-            player_red = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[10]'.format(n))
+        with open('scraped_data/{}_{}.csv'.format(group, season), 'w') as outfile:
+            outfile.write('playerName,team,games,goalsPerGame,yellowCards,2mins,redCards\n')
+            outfile.close()
+
+        for n in range(1, len(data.split('\n'))):
+            player_name = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[2]'.format(n))
+            player_team = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[3]'.format(n))
+            player_games = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[6]'.format(n))
+            player_gpg = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[7]'.format(n))
+            player_yellow = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[8]'.format(n))
+            player_2min = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[9]'.format(n))
+            player_red = driver.find_element_by_xpath(
+                '//*[@id="stats"]/div[2]/div[2]/table/tbody/tr[{}]/td[10]'.format(n))
 
             teams.append(player_team.text)
 
-            with open('scraped_data/{}_{}.csv'.format(group,season), 'ab') as outfile:
-
+            with open('scraped_data/{}_{}.csv'.format(group, season), 'ab') as outfile:
                 outfile.write(player_name.text.encode('utf-8'))
                 outfile.write(str.encode(',', 'utf-8'))
                 outfile.write(player_team.text.encode('utf-8'))
@@ -53,9 +68,10 @@ def scrape():
                 outfile.write(player_2min.text.encode('utf-8'))
                 outfile.write(str.encode(',', 'utf-8'))
                 outfile.write(player_red.text.encode('utf-8'))
-                outfile.write(str.encode('\n','utf-8'))
+                outfile.write(str.encode('\n', 'utf-8'))
                 outfile.close()
 
+        driver.quit()
     driver.quit()
 
 if __name__ == '__main__':
