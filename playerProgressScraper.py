eeeb1756be8dd6e51a37feb28c062a9a0aa6543a
taetscher <auteblauwau@hotmail.com>
2020-08-17 13:39:15 +0200

    closes issue #8  data now cleaned up and in readable format

--- playerProgressScraper.py
+++ playerProgressScraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import os
+import unicodecsv as csv
+
 
 #be aware, the team id changes once per season!
 teams_seasons = {'U15Elite':['30639'],'U17Elite':['30635'],'U19Elite':['30371']}
@@ -11,7 +13,11 @@ def main():
 
     # run firefox webdriver from executable path of your choice
     driver = webdriver.Firefox(
-        executable_path=r'C:\Users\Benjamin Schüpbach\Desktop\Coding\geckodriver-v0.27.0-win64\geckodriver.exe')
+        #on pc
+        #executable_path=r'C:\Users\Benjamin Schüpbach\Desktop\Coding\geckodriver-v0.27.0-win64\geckodriver.exe')
+
+        #on mac
+        executable_path = r'/Users/benischuepbach/Desktop/Coding/sources/geckodriver')
 
     for value in teams_seasons.values():
         teams.extend(value)
@@ -20,7 +26,7 @@ def main():
     for team in teams:
         try:
             print('creating new directory for team {}...'.format(team))
-            os.makedirs("playerProgress_data/{}".format(team), exist_ok=False)
+            os.makedirs("playerProgress_data/{}/raw_data".format(team), exist_ok=False)
         except OSError:
             print('directory for team {} already exists, skipping...'.format(team))
 
@@ -29,9 +35,9 @@ def main():
 
         for game in games:
             link = 'https://www.handball.ch/de/matchcenter/spiele/{}'.format(game)
-            game_stats, date = scrapeGame(link, team_name, driver)
-            writer(game_stats, game, date, team)
-            print(game_stats)
+            game_stats, date, league = scrapeGame(link, team_name, driver)
+            writer(game_stats, game, date, team, league)
+            print(date, team, league, game_stats)
 
     print('scraping successfully terminated, closing firefox...')
     driver.quit()
@@ -96,30 +102,51 @@ def scrapeGame(link,team,driver):
     left_table = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[3]/div[1]/div/table')
     right_table = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[3]/div[2]')
 
-    left_content = left_table.text
+    left_content = left_table.get_attribute('innerText')
     left_team = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[3]/div[1]/div/table/thead[1]/tr/td/span').text
     time.sleep(0.5)
 
-    right_content = right_table.text
+    right_content = right_table.get_attribute('innerText')
     right_team = driver.find_element_by_xpath('//*[@id="stats"]/div[2]/div[3]/div[2]/div/table/thead[1]/tr/td/span').text
     time.sleep(0.5)
 
+    league = driver.find_element_by_xpath('/html/body/div[3]/div[1]/div[2]/div/div/div[6]/p').text
+    league = league.split('-')[1]
+
     if left_team.upper() == team:
-        return left_content, date
+        return left_content, date, league
     elif right_team.upper() == team:
-        return right_content, date
+        return right_content, date, league
     else:
         print('\n\nsomething went wrong, shutting down...')
         pass
 
-def writer(game_stats,game,date,team):
+def writer(game_stats,game,date,team, league):
     """helper function. writes statistics of input game-id into csv file"""
 
-    with open('playerProgress_data/{}/raw_{}.csv'.format(team,game),'w') as outfile:
-        #encode/decode to avoid characters being saved wrongly
-        outfile.write(date+'\n')
-        csv = game_stats.replace(' ', ',').encode().decode('cp1252')
-        outfile.write(csv)
+    with open('playerProgress_data/{}/raw_data/raw_{}.csv'.format(team,game),'wb') as outfile:
+        #write in bytes mode ('wb') to avoid characters being saved wrongly
+        print('\n\nwriting stats for game {}...'.format(game))
+        writeR = csv.writer(outfile)
+        date = date.split(' ')
+        league = league.split(' ')[1:]
+        writeR.writerow(date)
+        writeR.writerow(league)
+
+        cSv = game_stats.split('\n')
+        print(cSv)
+
+        for element in cSv:
+            # remove whitespace at beginning of strings
+            try:
+                element = element.strip().replace('\t',' ')
+                print(element)
+            except:
+                pass
+
+            element = element.split(' ')
+            writeR.writerow(element)
+
         outfile.close()
 
 if __name__ == '__main__':
--- plotterProgress.py
+++ plotterProgress.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 data_dir = 'playerProgress_data'
 
@@ -6,16 +7,43 @@ def main():
     folders = os.listdir(data_dir)
 
     for folder in folders:
+        print(folder)
         #get all files in folder
         files = []
-        files.extend(os.listdir('{}/{}'.format(data_dir, folder)))
+        files.extend(os.listdir('{}/{}/raw_data'.format(data_dir, folder)))
 
-        #convert to readable csv
+        #convert csv files to account for player info and goalie info
         for file in files:
-            print('\nreading file {}...\n'.format(file))
+            print('\nconverting file {}...\n'.format(file))
             csvConverter(file,folder)
 
+        #read in newly generated, split csvs
+        inputs = []
+        outfield = []
+        goalies = []
+
+        inputs.extend(os.listdir('{}/{}'.format(data_dir,folder)))
+
+        #create list of files for outfield/goalie players
+        for file in inputs:
+            if str(file).split('_')[-1] == 'outfield.csv':
+                outfield.append(file)
+            elif str(file).split('_')[-1] == 'goalies.csv':
+                goalies.append(file)
+
+        print(outfield)
         #do pandas calculations/joins to be able to have each game as a column, each player as a row
+            #create empty dataframe to which to join all of the information of each file
+        outfield_df = pd.read_csv('{}/{}/{}'.format(data_dir,folder,outfield[0]),engine='python')
+        goalies_df = pd.DataFrame()
+
+        for file in outfield[1:]:
+            temp_df = pd.read_csv('{}/{}/{}'.format(data_dir,folder,file))
+            print(temp_df)
+            outfield_df.join(temp_df,on='SPIELER')
+
+        print(outfield_df.head)
+
 
         #plot time series data of each player to see his/her progress over time
 
@@ -23,14 +51,93 @@ def csvConverter(infile,folder):
     """ takes in messy raw data and turns it into readable csv format
 
     writes one output file for outfield players and one file for goalies"""
+    game_number = infile[4:-4]
 
-    with open('{}/{}/{}'.format(data_dir,folder,infile), 'r') as infile:
-        lines = infile.readlines()
-        date = lines[0].split(' ')[1]
-        header = lines[2]
-        
-        print(lines)
-        print(date,header)
+    data = []
+    with open('{}/{}/raw_data/{}'.format(data_dir,folder,infile), 'rb') as infile:
+
+        for line in infile.readlines():
+            line = line.decode('utf-8')
+            line = line[:-2]
+            data.append(line.split(' '))
+
+
+
+    date = cleanUp(data[0]).split(' ')[1].replace('.','_')
+    date = date[-2:] + '_' + date[3:5] + '_' + date[:2]
+    group = cleanUp(data[1])
+    team = cleanUp(data[2]) + cleanUp(data[1])
+    header_players = str(data[3]).strip('["').strip('"]')
+
+    goalie_index = float('NaN')
+    staff_index = float('NaN')
+
+    #find where to slice the data to get player stats
+    for entry in data:
+        if "'TORHÜTER,P/W,7M,%'" in str(entry):
+            index = data.index(entry)
+            goalie_index = index + 1
+        else:
+            pass
+
+    # find where to slice the data to get goalie stats
+    for entry in data:
+        if "STAFF,V,2',D" in str(entry):
+            index = data.index(entry)
+            staff_index = index + 1
+        else:
+            pass
+
+    player_data = data[4:goalie_index-2]
+    goalie_data = data[goalie_index:staff_index-2]
+    header_goalies = cleanUp(data[goalie_index-1]).replace(' ',',')
+
+
+    #write the cleaned data into two seperate files
+    #outfield players
+    with open('playerProgress_data/{}/{}_game{}_outfield.csv'.format(folder,date,game_number),'w',encoding='utf-8') as outfile:
+        outfile.write(header_players +'\n')
+        for element in player_data:
+            element = element[0].split(',')
+            element = str(element[1:]).strip("[").strip("]")
+
+            player_names = eval(element)[:-7]
+            player_name = ''
+            for segment in player_names:
+                player_name = player_name.strip() + ' ' + segment
+
+            player_stats_in = eval(element)[-7:]
+            player_stats = []
+            for stat in player_stats_in:
+                player_stats.append(str(stat))
+
+            element = str(player_name) + ',' + str(player_stats).strip('[').strip(']').replace("'",'').replace(' ','')
+            outfile.write(element +'\n')
+        outfile.close()
+
+    #goalies
+    with open('playerProgress_data/{}/{}_game{}_goalies.csv'.format(folder,date,game_number),'w',encoding='utf-8') as outfile:
+        outfile.write(header_goalies + '\n')
+        for element in goalie_data:
+            element = element[0].split(',')
+            element = str(element[1:]).strip("[").strip("]")
+
+            goalie_names = eval(element)[:-3]
+            goalie_name = ''
+            for segment in goalie_names:
+                goalie_name = goalie_name.strip() + ' ' + segment
+
+            goalie_stats_in = eval(element)[-3:]
+            goalie_stats = []
+            for stat in goalie_stats_in:
+                goalie_stats.append(str(stat))
+
+            element = str(goalie_name) + ',' + str(goalie_stats).strip('[').strip(']').replace("'", '').replace(' ', '')
+            outfile.write(element + '\n')
+        outfile.close()
+
+def cleanUp(inlist):
+    return str(inlist).strip("['").strip("']").replace(',',' ')
 
 if __name__ == '__main__':
     main()
