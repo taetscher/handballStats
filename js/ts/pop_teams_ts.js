4cc83a2c5ccfd2ec56d4c273c03a9eeaa1be3ceb
Benjamin Schüpbach <auteblauwau@hotmail.com>
2020-12-27 13:10:49 +0100

    cursor:pointer

--- js/ts/pop_teams_ts.js
+++ js/ts/pop_teams_ts.js
@@ -3,14 +3,14 @@ export function populateDropdownTS(tree, elementId){
     var x;
     for (x in keys){
         var team = keys[x];
-        var newA = document.createElement('li');
-        newA.setAttribute('href',"#");
-        newA.addEventListener('click', function() {
+        var newO = document.createElement('OPTION');
+        newO.setAttribute('href',"#");
+        newO.addEventListener('click', function() {
             var selected = this.innerHTML;
             console.log(selected);
             document.getElementById('dropdown_teams').innerHTML = selected
                         })
-        newA.innerHTML = team;
-        document.getElementById('teams').appendChild(newA);
+        newO.innerHTML = team;
+        document.getElementById('teams').appendChild(newO);
     }
 }
